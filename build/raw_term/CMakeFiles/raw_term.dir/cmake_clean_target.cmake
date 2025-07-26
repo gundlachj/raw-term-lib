9f505f7b511f165a44bd67a666e6dc940a1032e2
gundlachj <gundlachj@msoe.edu>
2025-07-25 20:54:36 -0400

    added library project structure

---
+++ build/raw_term/CMakeFiles/raw_term.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libraw_term.a"
+)
---
+++ example/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "raw_term/raw_term.hpp"
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Hello World!" << '\n';
+  return 0;
+}
